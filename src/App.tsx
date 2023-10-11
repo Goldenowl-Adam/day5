7a1144e3a3c77db0bc979d3a387e1d6df042caae
Adam Ho <adam.ho.goldenowl@gmail.com>
2023-10-11 17:50:28 +0700

    done day5

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import './index.scss';
+
+function App() {
+    const [isOpenInput, setIsOpenInput] = useState(false);
+
+    return (
+        <div className="container-search-box">
+            <div className={'search-box ' + (isOpenInput ? 'input-width' : '')}>
+                <input className="search-box__input" type="text" />
+                <button
+                    className="search-box__btn"
+                    onClick={() => {
+                        setIsOpenInput(!isOpenInput);
+                    }}
+                >
+                    <i className="fa-solid fa-magnifying-glass" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
